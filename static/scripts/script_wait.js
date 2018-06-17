7350123c841560da464ea24158a7e16614e44b55
Fr1m3n <34315303+Fr1m3n@users.noreply.github.com>
2018-06-17 15:57:39 +0300

    Update script_wait.js

--- static/scripts/script_wait.js
+++ static/scripts/script_wait.js
@@ -6,7 +6,7 @@ let i = date.getTime() - now_date.getTime();
 setInterval(function(){
     let now_date = new Date();
     now_date.setHours(now_date.getHours() + 3);
-    let i = Math.abs(date.getTime() - now_date.getTime());
+    i = Math.abs(date.getTime() - now_date.getTime());
     let q = document.getElementById("tid");
     // console.log(now_date);
     if(i <= 0){
