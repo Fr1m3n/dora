7f8e72d40911984ed277c31a9d3fe116b3a2f6d9
Fr1m3n <34315303+Fr1m3n@users.noreply.github.com>
2018-06-27 23:10:29 +0300

    edited text

--- static/scripts/script_main.js
+++ static/scripts/script_main.js
@@ -12,22 +12,22 @@ slides[0].color = 'rgb(255, 255, 204)';
 slides[1].text = 'Вот тебе и 16 годиков.';
 slides[1].type = 'Text';
 slides[1].color = 'rgb(255, 255, 153)';
-slides[2].text = 'Я невероятно счастлив, что встречаюсь с тобой <3';
+slides[2].text = 'Я безумно счастлив, что ты моя девушка <3';
 slides[2].type = 'Text';
 slides[2].color = 'rgb(255, 255, 102)';
 slides[3].text = 'Ты правда невероятная.';
 slides[3].type = 'Text';
 slides[3].color = 'rgb(255, 255, 51)';
-slides[4].text = 'Я очень хочу чтобы у тебя всё было великолепно.';
+slides[4].text = 'Я очень хочу чтобы у тебя всё было хорошо.';
 slides[4].type = 'Text';
 slides[4].color = 'rgb(255, 255, 0)';
-slides[5].text = 'И чтобы ты тоже была счастлива.';
+slides[5].text = 'И чтобы ты тоже была счастлива(как я).';
 slides[5].type = 'Text';
 slides[5].color = 'rgb(255, 212, 0)';
 slides[6].text = 'Хоть я и уеду скоро, но уверен - время пролетит.';
 slides[6].type = 'Text';
 slides[6].color = 'rgb(153, 255, 204)';
-slides[7].text = 'И мы снова встретимся с:';
+slides[7].text = 'Потом я вернусь и всё снова будет хорошо с:';
 slides[7].type = 'Text';
 slides[7].color = 'rgb(102, 255, 204)';
 slides[8].text = '<p> Це ти :з </p>'+
@@ -38,27 +38,29 @@ slides[8].color = 'rgb(255, 255, 163)';
 slides[9].text = 'Хочется, чтобы этот день для тебя был особенным.';
 slides[9].type = 'Text';
 slides[9].color = 'rgb(0, 255, 154)';
-slides[10].text = 'Всё таки у тебя день рождения!';
+slides[10].text = 'Это день рождения как никак!';
 slides[10].type = 'Text';
 slides[10].color = 'rgb(51, 204, 105)';
-slides[11].text = 'Тебя окружают прекрасные люди.';
+slides[11].text = 'Я тебе уже говорил, что ты очень красивая?';
 slides[11].type = 'Text';
 slides[11].color = 'rgb(0, 204, 102)';
-slides[12].text = 'И мы всегда готовы тебе помочь.';
+slides[12].text = 'И безумно милая :з';
 slides[12].type = 'Text';
 slides[12].color = 'rgb(204, 255, 51)';
-slides[13].text = 'Так что никогда не бойся просить (особенно у меня) о чём то.';
+slides[13].text = 'Ты очень для меня дорога.';
 slides[13].type = 'Text';
 slides[13].color = 'rgb(255, 100, 51)';
 slides[14].text = 'Я тебя люблю.';
 slides[14].type = 'Text';
 slides[14].color = 'rgb(255, 51, 0)';
-slides[15].text = '<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">'+
+slides[15].text = '<p> Спасибо тебе за всё </p>' +
+ '<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">'+
  '<g class="lines">'+
   '<title>Layer 1</title>'+
   '<path fill="none" stroke="#ff0000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" d="m294.63489,173.13809c62.46265,-163.84241 307.20013,0 0,210.65414c-307.19644,-210.65414 -62.46045,-374.49656 0,-210.65414z" id="svg_6"/>'+
  '</g>'+
-'</svg>';
+'</svg>'+
+ '<p> С днём рождения! </p>';
 slides[15].type = 'SVG';
 slides[15].color = 'rgb(248, 0, 0)';
 
